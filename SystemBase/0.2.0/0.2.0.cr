2c9d8dc9eb430ea379db1fe5aaad078088d3d210
Fulgurance <zohran.londais@gmail.com>
2024-01-16 18:23:18 +0000

    Remove obsolete instruction for user and group files

--- SystemBase/0.2.0/0.2.0.cr
+++ SystemBase/0.2.0/0.2.0.cr
@@ -87,45 +87,6 @@ class Target < ISM::Software
             CODE
             fileWriteData("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}etc/hosts",hostsData)
 
-            passwdData = <<-CODE
-            root:x:0:0:root:/root:/bin/bash
-            bin:x:1:1:bin:/dev/null:/bin/false
-            daemon:x:6:6:Daemon User:/dev/null:/bin/false
-            messagebus:x:18:18:D-Bus Message Daemon User:/run/dbus:/bin/false
-            uuidd:x:80:80:UUID Generation Daemon User:/dev/null:/bin/false
-            nobody:x:99:99:Unprivileged User:/dev/null:/bin/false
-            CODE
-            fileWriteData("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}etc/passwd",passwdData)
-
-            groupData = <<-CODE
-            root:x:0:
-            bin:x:1:daemon
-            sys:x:2:
-            kmem:x:3:
-            tape:x:4:
-            tty:x:5:
-            daemon:x:6:
-            floppy:x:7:
-            disk:x:8:
-            lp:x:9:
-            dialout:x:10:
-            audio:x:11:
-            video:x:12:
-            utmp:x:13:
-            usb:x:14:
-            cdrom:x:15:
-            adm:x:16:
-            messagebus:x:18:
-            input:x:24:
-            mail:x:34:
-            kvm:x:61:
-            uuidd:x:80:
-            wheel:x:97:
-            nogroup:x:65534:
-            users:x:999:
-            CODE
-            fileWriteData("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}etc/group",groupData)
-
             @@emptyFiles.each do |file|
                 generateEmptyFile("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}#{file}")
             end
