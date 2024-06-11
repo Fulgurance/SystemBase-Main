79734a48380a0eabbcf0b202db6fe9c65b7ef965
Fulgurance <zohran.londais@gmail.com>
2024-06-11 15:24:53 +0100

    Updated for next ism release

--- Kernel/0.2.0/0.2.0.cr
+++ Kernel/0.2.0/0.2.0.cr
@@ -1,2 +1,39 @@
 class Target < ISM::VirtualSoftware
+
+    def configure
+        super
+
+        if option("Pass1")
+            makeSource(["mrproper"], path: mainKernelSourcesPath)
+        end
+    end
+
+    def build
+        super
+
+        if option("Pass1")
+            makeSource(["headers"], path: mainKernelSourcesPath)
+        end
+    end
+
+    def prepareInstallation
+        super
+
+        #Make link for the current kernel documentation
+        makeLink("/usr/src/main-kernel-sources/Documentation", "/usr/share/doc/main-kernel-documentation", :symbolicLinkByOverwrite)
+
+        #Make link for the current running kernel sources
+        makeLink("/usr/src/#{mainKernelName}", "/usr/src/main-kernel-sources", :symbolicLinkByOverwrite)
+
+        linkKernelHeaders
+    end
+
+    def install
+        super
+
+        if !option("Pass1")
+            updateKernelOptionsDatabase
+        end
+    end
+
 end
