f1b32ae89df1edd05f1d20895fbeec3615207c1b
Fulgurance <zohran.londais@gmail.com>
2024-06-11 20:38:28 +0100

    Finished to implement new experimental installer

--- Kernel/0.2.0/0.2.0.cr
+++ Kernel/0.2.0/0.2.0.cr
@@ -1,40 +1,59 @@
 class Target < ISM::VirtualSoftware
 
-    def configure
-        super
-
-        if option("Pass1")
-            makeSource(["mrproper"], path: mainKernelSourcesPath)
-        end
-    end
-
-    def build
-        super
-
-        if option("Pass1")
-            makeSource(["headers"], path: mainKernelSourcesPath)
-        end
-    end
-
-    def prepareInstallation
-        super
-
-        #Make link for the current kernel documentation
-        makeLink("/usr/src/main-kernel-sources/Documentation", "/usr/share/doc/main-kernel-documentation", :symbolicLinkByOverwrite)
-
-        #Make link for the current running kernel sources
-        makeLink("/usr/src/#{mainKernelName}", "/usr/src/main-kernel-sources", :symbolicLinkByOverwrite)
-    end
-
     def install
         super
 
-        linkKernelHeaders
-        #Needed ?
-        #Record links and dirs ?
-        #if !option("Pass1")
-            updateKernelOptionsDatabase
-        #end
+        #Generate headers
+        makeSource(["mrproper"], path: mainKernelSourcesPath)
+        makeSource(["headers"], path: mainKernelSourcesPath)
+
+        #Make link for the current kernel documentation
+        makeLink("/usr/src/main-kernel-sources/Documentation", "/usr/share/doc/main-kernel-documentation", :symbolicLinkByOverwrite)
+        #Make link for the current running kernel sources
+        makeLink("/usr/src/#{mainKernelName}", "/usr/src/main-kernel-sources", :symbolicLinkByOverwrite)
+
+        ################################################################################
+        #This method is temporary before a proper one is implemented to get directories#
+        ################################################################################
+
+        #Generate symlinks of the current kernel headers to /usr/include
+        headerDirectories = [   "acpi",
+                                "asm-generic",
+                                "clocksource",
+                                "crypto",
+                                "drm",
+                                "dt-bindings",
+                                "generated",
+                                "keys",
+                                "kunit",
+                                "kvm",
+                                "linux",
+                                "math-emu",
+                                "media",
+                                "memory",
+                                "misc",
+                                "net",
+                                "pcmia",
+                                "ras",
+                                "rdma",
+                                "rv",
+                                "scsi",
+                                "soc",
+                                "sound",
+                                "target",
+                                "trace",
+                                "uapi",
+                                "ufs",
+                                "vdso",
+                                "video",
+                                "xen"]
+
+        headerDirectories.each do |headerDirectory|
+            makeLink("/usr/src/#{mainKernelName}/include/#{headerDirectory}", "/usr/include/#{headerDirectory}", :symbolicLinkByOverwrite)
+        end
+
+        #Experimental
+        #updateKernelOptionsDatabase
     end
 
 end
