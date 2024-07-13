47b2f2b2cab18286eb03340aa7fd137c9dbfbc4f
Fulgurance <zohran.londais@gmail.com>
2024-07-13 21:48:11 +0100

    Finished to implement properly Kernel software

--- Kernel/0.2.0/0.2.0.cr
+++ Kernel/0.2.0/0.2.0.cr
@@ -25,40 +25,17 @@ class Target < ISM::VirtualSoftware
         makeSource( arguments:  "headers",
                     path: "#{Ism.settings.rootPath}/usr/src/main-kernel-sources")
 
-        ################################################################################
-        #This method is temporary before a proper one is implemented to get directories#
-        ################################################################################
-
         #Generate symlinks of the current kernel headers to /usr/include
-        headerDirectories = [   "asm",
-                                "asm-generic",
-                                "drm",
-                                "linux",
-                                "misc",
-                                "mtd",
-                                "rdma",
-                                "regulator",
-                                "scsi",
-                                "sound",
-                                "video",
-                                "xen"]
-
-        # headerDirectories.each do |headerDirectory|
-        #     makeLink(   target: "../src/main-kernel-sources/usr/include/#{headerDirectory}",
-        #                 path:   "#{Ism.settings.rootPath}/usr/include/#{headerDirectory}",
-        #                 type:   :symbolicLinkByOverwrite)
-        # end
+        headerPath = "#{Ism.settings.rootPath}/usr/src/main-kernel-sources/usr/include/"
+        headerDirectories = Dir.children(headerPath).select { |entry| File.directory?(entry) }
 
         headerDirectories.each do |headerDirectory|
-            copyDirectory(  "#{Ism.settings.rootPath}/usr/src/main-kernel-sources/usr/include/#{headerDirectory}",
-                            "#{Ism.settings.rootPath}/usr/include/#{headerDirectory}")
-        #     makeLink(   target: "../src/main-kernel-sources/usr/include/#{headerDirectory}",
-        #                 path:   "#{Ism.settings.rootPath}/usr/include/#{headerDirectory}",
-        #                 type:   :symbolicLinkByOverwrite)
+             makeLink(   target: "../src/main-kernel-sources/usr/include/#{headerDirectory}",
+                         path:   "#{Ism.settings.rootPath}/usr/include/#{headerDirectory}",
+                         type:   :symbolicLinkByOverwrite)
         end
 
-        #Experimental
-        #updateKernelOptionsDatabase
+        updateKernelOptionsDatabase
     end
 
 end
