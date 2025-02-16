9015f40e92223017fca530ff41b399a8f81d6197
Fulgurance <zohran.londais@gmail.com>
2025-02-16 18:29:01 +0000

    Updated installation process

--- Kernel/0.2.0/0.2.0.cr
+++ Kernel/0.2.0/0.2.0.cr
@@ -2,30 +2,8 @@ class Target < ISM::VirtualSoftware
 
     def install
         super
-
         recordSelectedKernel
         updateKernelSymlinks
-
-        makeDirectory("#{Ism.settings.rootPath}/usr/share/doc/")
-        makeDirectory("#{Ism.settings.rootPath}/usr/include")
-
-        #Generate headers
-        makeSource( arguments:   "clean",
-                    path: "#{Ism.settings.rootPath}/usr/src/#{mainKernelName}")
-
-        makeSource( arguments:  "headers",
-                    path: "#{Ism.settings.rootPath}/usr/src/#{mainKernelName}")
-
-        #Generate symlinks of the current kernel headers to /usr/include
-        headerPath = "#{Ism.settings.rootPath}/usr/src/main-kernel-sources-headers/usr/include/"
-        headerDirectories = Dir.children(headerPath).select { |entry| File.directory?("#{headerPath}/#{entry}") }
-
-        headerDirectories.each do |headerDirectory|
-             makeLink(   target: "../src/main-kernel-sources-headers/#{headerDirectory}",
-                         path:   "#{Ism.settings.rootPath}/usr/include/#{headerDirectory}",
-                         type:   :symbolicLinkByOverwrite)
-        end
-
         updateKernelOptionsDatabase
     end
 
