c7ad64b9e200376e75ec9d5ee90034cab214afcf
Fulgurance <zohran.londais@gmail.com>
2025-05-15 15:37:19 +0100

    Updated help message

--- ChrootBase/0.2.0/0.2.0.cr
+++ ChrootBase/0.2.0/0.2.0.cr
@@ -4,12 +4,16 @@ class Target < ISM::VirtualSoftware
         super
 
         showInfo("Now the system is able to handle the chroot.")
-        showInfo("Firstly, mount all necessary virtual kernel filesystems by the following commands:")
-        showInfoCode("mount --types proc /proc /mnt/ism/proc")
-        showInfoCode("mount --rbind /sys /mnt/ism/sys")
-        showInfoCode("mount --rbind /dev /mnt/ism/dev")
-        showInfoCode("mount --bind /run /mnt/ism/run ")
-        showInfo("Then, you can enable the secure mode and continue your installation.")
+        showInfo("Firstly, set the owners as root for the new system:")
+        showInfoCode("sudo chown -R /mnt/ism")
+        showInfo("After this, mount all necessary virtual kernel filesystems by the following commands:")
+        showInfoCode("sudo mount --types proc /proc /mnt/ism/proc")
+        showInfoCode("sudo mount --rbind /sys /mnt/ism/sys")
+        showInfoCode("sudo mount --rbind /dev /mnt/ism/dev")
+        showInfoCode("sudo mount --bind /run /mnt/ism/run")
+        showInfo("Then copy the network configuration to the new system:")
+        showInfoCode("sudo cp /etc/resolv.conf /mnt/ism/etc/resolv.conf")
+        showInfo("Finally, you can enable the secure mode and continue your installation.")
         showInfoCode("sudo ism settings -esm")
     end
 
